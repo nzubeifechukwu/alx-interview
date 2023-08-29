2fef61a9fb4cd333e9530bf98a13822363ac6d50
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-08-29 20:52:25 +0100

    Fix documentation issues

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-'''Island perimeter'''
+'''Island perimeter Python interview solution'''
 
 
 def island_perimeter(grid):
+    '''Island perimeter
+
+    Args: @grid is a 2d array possibly representing an island
+
+    Return: the island perimeter
+    '''
     row_len = len(grid)
     col_len = len(grid[0])
     perimeter = 0
